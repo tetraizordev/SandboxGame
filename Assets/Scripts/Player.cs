af033f994898fa530832774ac53d12e236dfd086
Umuthan Özel <ozelumuthan@gmail.com>
2021-11-23 21:00:27 +0300

    Init.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Values")]
+    public float walkSpeed = 200;
+    public float maxHealth = 100;
+    public float currentHealth;
+    float runSpeedMultiplier = 1.8f;
+    float speed;
+
+    [Header("Input")]
+    [SerializeField]
+    [Range(-1, 1)]
+    float horizontalInput;
+    [Range(-1, 1)]
+    [SerializeField]
+    float verticalInput;
+
+    [Header("States")]
+    
+    [SerializeField]
+    bool canMove;
+    [SerializeField]
+    bool isRunning;
+
+    [Header("Assignments")]
+    Rigidbody2D playerRigidbody;
+    Vector2 moveDir;
+    void Awake()
     {
-        
+        playerRigidbody = gameObject.GetComponent<Rigidbody2D>();
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        isRunning = Input.GetButton("Run");
+
+        if(isRunning)
+        {
+            speed = walkSpeed * runSpeedMultiplier * 1000;
+        }
+        else
+        {
+            speed = walkSpeed * 1000;
+        }
         
+        moveDir = new Vector2(horizontalInput, verticalInput).normalized * speed;
+
+        playerRigidbody.AddForce(moveDir * Time.fixedDeltaTime);
     }
+
+
+
 }
